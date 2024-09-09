7e719999ce25bbcac51848a28af02c2469744d08
JongInHey <youyi1234@naver.com>
2024-09-09 21:56:04 +0900

    router & globalStyled setup

---
+++ src/GlobalStyled.js
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+export const GlobalStyled = createGlobalStyle`
+    ${reset}
+
+    * {box-sizing: border-box}
+
+    body {
+        font-family: "Noto Sans KR", sans-serif;
+        background-color: #dbdbdb;
+        color: #222;
+    }
+    
+    img {
+        display: block;
+        width: 100%;
+    }
+    
+    ul, li {
+        list-style: none;
+    }
+
+    a {
+        text-decoration: none;
+        color: #222;
+    }
+
+
+`;
